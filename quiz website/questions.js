747f5663d547c99e43912545ed83138c3d3b309e
priyal1015 <128025348+priyal1015@users.noreply.github.com>
2023-10-30 12:41:26 +0530

    Add files via upload

---
+++ quiz website/questions.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Which is the first search engine on internet?",
+        answer: "B. Archie",
+        options: [
+            "A. Google",
+            "B. Archie", 
+            "C. Altavista", 
+            "D. WAIS"
+
+        ]
+    },
+{
+    numb: 2,
+    question: "First computer virus is known as?",
+    answer: "B. Creeper virus",
+    options: [
+        "A. Rabbit", 
+        "B. Creeper virus", 
+        "C. ELK cloner", 
+        "D. SCA virus"
+    ]
+},
+{
+    numb: 3,
+    question: "Firewall in computer is used for?",
+    answer: "A. Security",
+    options: [
+        "A. Security", 
+        "B. Data Transmission", 
+        "C. Authentication", 
+        "D. Monitoring"
+    ]
+},
+{
+    numb: 4,
+    question: "Where is the headquarter of Microsoft office is located?",
+    answer: "D. Washington",
+    options: [
+        "A. Texas", 
+        "B. New York", 
+        "C. California", 
+        "D. Washington"
+    ]
+},
+{
+    numb: 5,
+    question: "Which one is the current fastest supercomputer in India?",
+    answer: "C. SahasraT",
+    options: [
+        "A. Aaditya", 
+        "B. SAGA-220", 
+        "C. SahasraT", 
+        "D. HP Apollo 6000"
+    ]
+} 
+];
